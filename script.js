651d5a59f537f439865a9d0de52db588c3fd2f0d
Shanmugapriya R K <priyachandranshan@gmail.com>
2025-05-13 11:45:23 +0530

    Add files via upload
    This code is for landing a food delivery page using HTML & CSS and javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript functionality
+document.querySelector('.btn').addEventListener('click', function() {
+  alert('Redirecting to menu...');
+  // Add functionality to navigate to the menu section
+});
